cd5e59406228c6fd9f0003c5ea4eb5927b9c50f3
Cameron Wilson <clamcambam@gmail.com>
2026-05-06 23:14:16 -0400

    Fix naming and sync structure

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import RosaryTrackerAppPrototype from './RosaryApp'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RosaryTrackerAppPrototype />
+  </React.StrictMode>,
+)
